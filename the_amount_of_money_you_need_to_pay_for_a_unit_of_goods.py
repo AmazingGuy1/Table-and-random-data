3bfc92b729dd1c6b176122c55372e6c2dbfeb77a
Amazing Guy <117029418+AmazingGuy1@users.noreply.github.com>
2022-11-06 16:22:11 +0300

    the amount of money you need to pay for a unit of goods

---
+++ the_amount_of_money_you_need_to_pay_for_a_unit_of_goods.py
@@ -0,0 +1,10 @@
+from time import sleep
+from tabulate import tabulate
+
+product_name = input('Введите имя продукта  ')
+sell_product = float(input('Введите стоимость продукта  '))
+k_product = int(input('Введите количетсво продукта  '))
+
+a = tabulate([['Названия продукта', product_name], ['Стоимость продукта', sell_product], ['Количество продукта', k_product], ['Итоговая стоимость', sell_product * k_product]], tablefmt="simple_outline")
+
+print(a)
