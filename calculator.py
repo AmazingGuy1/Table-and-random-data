37be55604aa81a5d77c72bceb5c1be8e31c613fb
Amazing Guy <117029418+AmazingGuy1@users.noreply.github.com>
2022-11-24 18:21:28 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Создания простока калькулятор. > арифметические действия (+ -)
+
+# Версия 1.0 RU
+
+number_of_iteration = int(input('Введите колличество итераций  '))
+counter = 0
+
+if len(str(number_of_iteration)) > 0:
+	
+    sum_of_numbers = int()
+    different_of_number = int()
+    quotient_of_numbers = int()
+    products_of_numbers = int()
+    
+    
+    while counter != number_of_iteration:
+    
+        _integer = int(input('Введите число  '))
+
+        sum_of_numbers += _integer
+        different_of_number -= _integer
+        
+        counter += 1
+    
+    print(sum_of_numbers, different_of_number, sep='\n')
+      
+
+
+else:
+    print('Программа окончилась неудачно')
